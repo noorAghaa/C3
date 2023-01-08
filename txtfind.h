6e0715434744a8ffb676569be9ec4c8834de85aa
noorAghaa <120051245+noorAghaa@users.noreply.github.com>
2023-01-08 15:20:17 +0200

    Update txtfind.h

--- txtfind.h
+++ txtfind.h
@@ -1,9 +1,10 @@
-#define LINE 256
-#define WORD 30
+#ifndef STRINGC_H
+#define STRINGC_H
 
-int getLine(char s[]);
-int getWord(char w[]);
+int ggetline(char str[]);
+int getword(char str[]);
 int substring( char * str1, char * str2);
 int similar (char *s, char *t, int n);
 void print_lines(char * str);
 void print_similar_words(char * str);
+#endif
